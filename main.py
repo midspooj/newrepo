0d14527fc870f17c7cf1f38f3db85ce18a79d060
midspooj <142264378+midspooj@users.noreply.github.com>
2023-09-03 18:12:43 +0000

    python mini-project

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-"""
-Main code
-"""
+# Function to generate Fibonacci sequence up to n numbers
+def generate_fibonacci(n):
+    fibonacci_sequence = [0, 1]  # Initialize the sequence with the first two Fibonacci numbers
 
+    while len(fibonacci_sequence) < n:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
 
-def negative_number(a):
-    if a < 0:
-        return True
+    return fibonacci_sequence
 
+# Get user input for the number of Fibonacci numbers to generate
+n = int(input("Enter the number of Fibonacci numbers to generate: "))
+
+# Generate and print the Fibonacci sequence
+fibonacci_numbers = generate_fibonacci(n)
+print("Fibonacci Sequence (first", n, "numbers):")
+print(fibonacci_numbers)
---
+++ testpython.py
@@ -0,0 +1,13 @@
+# Sample list of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Initialize a variable to store the sum
+sum_of_numbers = 0
+
+# Iterate through the list and add each element to the sum
+for num in numbers:
+    sum_of_numbers += num
+
+# Print the sum
+print("Sum of the numbers:", sum_of_numbers)
+
